e42140e11a3485821d5aaa7a3e8fea432b5a7448
Kang Dong Hyeon <ehdgus6435@kumoh.ac.kr>
2025-05-10 19:06:19 +0900

    init : 프로젝트 생성 및 GitHub 이슈 템플릿 추가
    - 프로젝트 생성
    - 버그 리포트 템플릿 (버그 발생 상황 및 재현 방법 기록)
    - 기능 요청 템플릿 (새로운 기능 제안 및 개발 계획)
    - 토론/아이디어 템플릿 (아이디어 기록 및 의사결정 과정 문서화)
    - 일반 작업 템플릿 (리팩토링, 문서화, 설정 변경 등)

---
+++ board-server/src/test/java/com/fastcampus/board_server/BoardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.board_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
